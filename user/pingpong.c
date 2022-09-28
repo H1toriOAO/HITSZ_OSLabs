601ded92643f4e80c11c71e9f52e44b5095372fe
stardustcyan <1638876113@qq.com>
2022-09-28 10:46:21 +0000

    before checkout

--- user/pingpong.c
+++ user/pingpong.c
@@ -1,52 +1,58 @@
 #include "kernel/types.h"
 #include "user.h"
 
-int main(int argc, char* argv[]) {
-    if (argc != 1) {        // 校验参数数量
+int main(int argc, char* argv[]) 
+{
+    if (argc != 1) 
+    {        // 校验参数数量
         printf("PINGPONG NEEDS NO ARGUMENTS!\n");
         exit(-1);
     }
 
-    int p[2];       // 创建管道
-    pipe(p);
+    int p_father[2], p_child[2];       // 创建管道
+    pipe(p_father);
+    pipe(p_child);
 
-    int ret = fork();       // 创建子线程
     char msg[4];
-    if (ret == 0) {         // 子线程
-        int pid = getpid();
-        int r_len = read(p[0], msg, 4);
-        if (r_len != 4) {
+    if (fork() == 0)         // 子线程
+    {   
+        if (read(p_father[0], msg, 4) != 4) 
+        {
             printf("UNABLE TO READ PING!\n");
             exit(-1);
-        } else {
-            printf("%d: reveived ping\n", pid);
+        } 
+        else 
+        {
+            printf("%d: received ping\n", getpid());
         }
-        sleep(5);
 
         strcpy(msg, "pong");
-        int w_len = write(p[1], msg, 4);
-        if (w_len != 4) {
+        if (write(p_child[1], msg, 4) != 4) 
+        {
             printf("UNABLE TO WRITE PONG!\n");
             exit(-1);
         }
 
         exit(0);
-    } else {        // 父线程
-        int pid = getpid();
+    }
+    else         // 父线程
+    {
         strcpy(msg, "ping");
-        int w_len = write(p[1], msg, 4);
-        if (w_len != 4) {
+        
+        if (write(p_father[1], msg, 4) != 4) 
+        {
             printf("UNABLE TO WRITE PING!\n");
             exit(-1);
         }
-        sleep(5);
 
-        int r_len = read(p[0], msg, 4);
-        if (r_len != 4) {
+        if (read(p_child[0], msg, 4) != 4) 
+        {
             printf("UNABLE TO READ PONG!\n");
             exit(-1);
-        } else {
-            printf("%d: received pong\n", pid);
+        } 
+        else 
+        {
+            printf("%d: received pong\n", getpid());
         }
 
         exit(0);
--- user/sleep.c
+++ user/sleep.c
@@ -1,7 +1,7 @@
 #include "kernel/types.h"
 #include "user.h"
 
-int main(int argc,char* argv[]){
+int main(int argc, char *argv[]) {
     if(argc != 2){
         printf("Sleep needs one argument!\n"); //检查参数数量是否正确
         exit(-1);
