144a0c714c8f12ac8c4bc7dc24fdfaaed70712e1
stardustcyan <1638876113@qq.com>
2022-09-28 13:40:54 +0000

    实现了trace

--- kernel/proc.c
+++ kernel/proc.c
@@ -126,6 +126,7 @@ found:
   memset(&p->context, 0, sizeof(p->context));
   p->context.ra = (uint64)forkret;
   p->context.sp = p->kstack + PGSIZE;
+  p -> syscall_trace = 0;
 
   return p;
 }
@@ -290,7 +291,7 @@ fork(void)
   np->cwd = idup(p->cwd);
 
   safestrcpy(np->name, p->name, sizeof(p->name));
-
+    np -> syscall_trace = p -> syscall_trace;
   pid = np->pid;
 
   np->state = RUNNABLE;
