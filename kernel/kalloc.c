910d32f89ef64431654e66dcbdab74d484e2d0b6
stardustcyan <1638876113@qq.com>
2022-10-21 12:32:06 +0000

    1. 修改了clang-format设置，现在不会打乱头文件顺序； 2. 实现了task1

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -9,74 +9,125 @@
 #include "riscv.h"
 #include "defs.h"
 
-void freerange(void *pa_start, void *pa_end);
+void freerange(void* pa_start, void* pa_end);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
 
 struct run {
-  struct run *next;
+    struct run* next;
 };
 
 struct {
-  struct spinlock lock;
-  struct run *freelist;
-} kmem;
+    struct spinlock lock;
+    struct run* freelist;
+    char lockName[7];
+} kmem[NCPU];
 
-void
-kinit()
+void kinit()
 {
-  initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+    for (int i = 0; i < NCPU; i++) {
+        snprintf(kmem[i].lockName, sizeof(kmem[i].lockName), "kmem_%d", i);
+        initlock(&kmem[i].lock, kmem[i].lockName);
+    }
+
+    freerange(end, (void*)PHYSTOP);
 }
 
-void
-freerange(void *pa_start, void *pa_end)
+void freerange(void* pa_start, void* pa_end)
 {
-  char *p;
-  p = (char*)PGROUNDUP((uint64)pa_start);
-  for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
-    kfree(p);
+    char* p;
+    p = (char*)PGROUNDUP((uint64)pa_start);
+    for (; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
+        kfree(p);
 }
 
 // Free the page of physical memory pointed at by v,
 // which normally should have been returned by a
 // call to kalloc().  (The exception is when
 // initializing the allocator; see kinit above.)
-void
-kfree(void *pa)
+void kfree(void* pa)
 {
-  struct run *r;
+    struct run* r;
 
-  if(((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
-    panic("kfree");
+    if (((uint64)pa % PGSIZE) != 0 || (char*)pa < end || (uint64)pa >= PHYSTOP)
+        panic("kfree");
 
-  // Fill with junk to catch dangling refs.
-  memset(pa, 1, PGSIZE);
+    // Fill with junk to catch dangling refs.
+    memset(pa, 1, PGSIZE);
 
-  r = (struct run*)pa;
+    r = (struct run*)pa;
+    push_off();
 
-  acquire(&kmem.lock);
-  r->next = kmem.freelist;
-  kmem.freelist = r;
-  release(&kmem.lock);
+    int cpuID = cpuid();
+    acquire(&kmem[cpuID].lock);
+
+    r->next = kmem[cpuID].freelist;
+    kmem[cpuID].freelist = r;
+
+    release(&kmem[cpuID].lock);
+    pop_off();
 }
 
 // Allocate one 4096-byte page of physical memory.
 // Returns a pointer that the kernel can use.
 // Returns 0 if the memory cannot be allocated.
-void *
-kalloc(void)
+void* kalloc(void)
 {
-  struct run *r;
+    struct run* r;
+    push_off();
 
-  acquire(&kmem.lock);
-  r = kmem.freelist;
-  if(r)
-    kmem.freelist = r->next;
-  release(&kmem.lock);
+    int cpuID = cpuid();
+    acquire(&kmem[cpuID].lock);
 
-  if(r)
-    memset((char*)r, 5, PGSIZE); // fill with junk
-  return (void*)r;
+    r = kmem[cpuID].freelist;
+    if (r) {
+        kmem[cpuID].freelist = r->next;
+    } else {
+        int successFlag = 0;
+
+        for (int i = 0; i < NCPU; i++) {
+            if (i == cpuID) {
+                continue;
+            }
+
+            acquire(&kmem[i].lock);
+
+            struct run* p = kmem[i].freelist;
+            if (p) {
+                struct run *fp = p, *pre = p;
+                while (fp && fp->next) {
+                    fp = fp->next->next;
+                    pre = p;
+                    p = p->next;
+                }
+
+                kmem[cpuID].freelist = kmem[i].freelist;
+                if (p == kmem[i].freelist) {
+                    kmem[i].freelist = 0;
+                } else {
+                    kmem[i].freelist = p;
+                    pre->next = 0;
+                }
+
+                successFlag = 1;
+            }
+
+            release(&kmem[i].lock);
+
+            if (successFlag) {
+                r = kmem[cpuID].freelist;
+                kmem[cpuID].freelist = r->next;
+
+                break;
+            }
+        }
+    }
+
+    release(&kmem[cpuID].lock);
+    pop_off();
+
+    if (r)
+        memset((char*)r, 5, PGSIZE); // fill with junk
+    return (void*)r;
 }
